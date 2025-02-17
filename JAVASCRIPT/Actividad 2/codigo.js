7a6b5ded24574942d4ea5f4b8207b6166b61e1f1
Sara Diaz Casamayor <166033911+SaraDC93@users.noreply.github.com>
2025-02-17 16:33:54 +0100

    javascript

--- JAVASCRIPT/Actividad 2/codigo.js
+++
@@ -1,9 +0,0 @@
-
-
-document.write("Código escrito en fichero externo<br>");
-
-document.write("Otro código escrito en fichero externo<br>");
-
-console.log("Mensaje a la consola");
-console.log("Nuevo mensaje en la consola");
-
